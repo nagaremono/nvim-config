5fc65b3e394247408a38bf4e2e3b1c0f66031b78
nagaremono <ediguruhdev@gmail.com>
2025-12-07 18:22:21 +0700

    disable previewers until telescope and lsp fix

---
+++ lua/plugins/telescope.lua
@@ -0,0 +1,15 @@
+return {
+  {
+    "nvim-telescope/telescope.nvim",
+    opts = {
+      pickers = {
+        find_files = {
+          previewer = false,
+        },
+        buffers = {
+          previewer = false,
+        },
+      },
+    },
+  },
+}
