ebb833cdbae6c2df2e67c2945990d63499b8c8f9
nagaremono <ediguruhdev@gmail.com>
2026-02-08 21:11:21 +0700

    disable kotlin constant error notify

---
+++ lua/plugins/noice.lua
@@ -0,0 +1,16 @@
+return {
+  "folke/noice.nvim",
+  event = "VeryLazy",
+  opts = {
+    routes = {
+      {
+        -- Disable annoying error from `kotlin_language_server`.
+        opts = { skip = true },
+        filter = {
+          event = "notify",
+          find = "^kotlin_language_server: %-32603: Internal error%.$",
+        },
+      },
+    },
+  },
+}
